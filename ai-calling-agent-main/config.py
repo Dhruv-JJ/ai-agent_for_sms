142b5e847109e0bc69a599b976aceb0836a969cf
Dhruv-JJ <dj0410joysar@gmail.com>
2025-03-15 22:47:41 +0530

    Initiazlize

---
+++ ai-calling-agent-main/config.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TWILIO_ACCOUNT_SID = "ACe94bbc7fc7326f5b7a4d725147cce4e5"
+TWILIO_AUTH_TOKEN = "3381da322b456b2ba8a9b0936f05af0a"
+TWILIO_PHONE_NUMBER = "+17343884049"
+PERSONAL_PHONE ="+919321796713"
+OPENAI_API_KEY= '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'
+NGROK_URL="https://5d7b-49-248-175-242.ngrok-free.app"
+
+MONGODB_URI='mongodb+srv://dj0410joysar:nXwW9D7KrbNg2gcM@cluster0.hnqf7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+
+FLASK_APP='5bea32f4ea2691e2598ea51e7dc12dba'
+FLASK_ENV='http://api.waqi.info/feed/shanghai/?token=demo'
